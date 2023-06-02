82e378cdcf2907d057af41cb27665446b7a92634
Emre Yilmaz <emresamiylmz@gmail.com>
2023-06-02 11:05:35 +0300

    02/06/23

---
+++ src/main/java/miniProject/Pizza/Emre.java
@@ -0,0 +1,16 @@
+package miniProject.Pizza;
+
+public class Emre {
+    public static void main(String[] args) {
+        System.out.println("Emre");
+        System.out.println("Emre");
+        System.out.println("Emre");
+        System.out.println("Emre");
+        System.out.println("Emre");
+        System.out.println("Emre");
+        System.out.println("Emre");
+        System.out.println("Emre");
+        System.out.println("Emre");
+        System.out.println("Emre");
+    }
+}
---
+++ src/main/java/miniProject/Pizza/Pizza.java
@@ -0,0 +1,9 @@
+package miniProject.Pizza;
+
+
+
+public class Pizza {
+
+
+}
+
