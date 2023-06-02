8a307c8e71734e043036f38c247092067d93849d
AydaAkçay <aydaakcay@gmail.com>
2023-06-02 11:47:23 +0300

    Ver01

--- src/main/java/miniProject/Pizza/Emre.java
+++ src/main/java/miniProject/Pizza/Emre.java
@@ -4,13 +4,17 @@ public class Emre {
     public static void main(String[] args) {
         System.out.println("Emre");
         System.out.println("Emre");
+        System.out.println("Ayda");
+        System.out.println("Ayda");
+        System.out.println("Ayda");
+        System.out.println("Ayda");
         System.out.println("Emre");
         System.out.println("Emre");
         System.out.println("Emre");
         System.out.println("Emre");
         System.out.println("Emre");
-        System.out.println("Emre");
-        System.out.println("Emre");
-        System.out.println("Emre");
+        System.out.println("Ayda");
+        System.out.println("Ayda");
+        System.out.println("Ayda");
     }
 }
